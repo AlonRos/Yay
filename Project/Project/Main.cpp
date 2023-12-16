8e47eac58a14a2e0e10112f0b703d7e83756ce40
AlonRos <alon.rosenperl@gmail.com>
2023-12-16 22:53:19 +0200

    find the virtual addresses of KeServiceDescriptorTable and KeServiceDescriptorTableShadow

--- Project/Project/Main.cpp
+++ Project/Project/Main.cpp
@@ -2,21 +2,21 @@
 #include <iostream>
 #include "ReadWrite.h"
 
+
 int main() {
 
+
     HANDLE device = CreateFileW(L"\\\\.\\NTIOLib_MB", GENERIC_READ | GENERIC_WRITE, NULL, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
-    
-    BYTE buffer[16] = "Alon";
 
-    if (writePhysicalMemory(device, (PVOID)0x1234, 1, 4, buffer)) {
-        printf("writing succeeded\n");
-    }
+    PVOID KiSystemCall64;
+    readMSR(device, 0xc0000082, (QWORD*)&KiSystemCall64); //0xc0000082
+    printf("%llx\n", KiSystemCall64);
 
-    if (readPhysicalMemory(device, (PVOID)0x1234, 1, 16, buffer)) {
 
-        for (BYTE b : buffer) {
-            printf("%02X ", b);
-        }
-    }
+    PVOID KeServiceDescriptorTable = (BYTE*)KiSystemCall64 + 0x374 + 0x9f0b4c;
+    PVOID KeServiceDescriptorTableShadow = (BYTE*)KiSystemCall64 + 0x37b + 0x8ebcc5;
+     
+
+    printf("%llx, %llx\n", KeServiceDescriptorTable, KeServiceDescriptorTableShadow);
 
 }
